b6638531eebffa0b58070dbce0ecdfcd4230a7d7
liutengfei <liutf@mybpm.net>
2022-05-16 00:50:57 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author ltf
+ * @description
+ * @create 2022-05-16 0:45
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
